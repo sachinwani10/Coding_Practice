24fa038f010c024161832d0af350442e9a5e5348
sachinwani10 <36121968+sachinwani10@users.noreply.github.com>
2019-08-19 15:32:36 -0500

    Update and rename Palindrome(centerMethod).py) to Palindrome.py

--- Palindrome.py
+++ Palindrome.py
@@ -13,3 +13,18 @@ def palindrome(s):
     return s1 == s2[::-1]
     
 print(palindrome("redder"))
+
+==========================================
+
+def palindrome(s):
+    n = len(s)
+    i, j = 0, n-1
+    
+    while i < j:
+        if s[i] != s[j]:
+            return False
+        i += 1
+        j -= 1
+    return True
+    
+print(palindrome("raxxr"))
