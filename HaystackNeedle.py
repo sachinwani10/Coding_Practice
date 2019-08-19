551256bad47ff567dc455b9089019466aebc219d
sachinwani10 <36121968+sachinwani10@users.noreply.github.com>
2019-08-19 16:01:15 -0500

    Update HaystackNeedle.py

--- HaystackNeedle.py
+++ HaystackNeedle.py
@@ -1,1 +1,11 @@
-
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        if not needle or haystack == needle:
+            return 0
+        i = 0
+        while i <= (len(haystack) - len(needle)):
+            if haystack[i: i+len(needle)] == needle:
+                return i
+            else:
+                i += 1
+        return -1
